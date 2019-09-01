6956678375226a9b4929756fc43ed0a83d5c4637
Ashoojain <ashoojain6@gmail.com>
2019-09-01 17:56:45 +0530

    sending email

--- mail.js
+++ mail.js
@@ -1,12 +1,19 @@
 const send1=require('@sendgrid/mail');
 
+// key of sendgrid account
 const key='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';
+
+// set the key
 send1.setApiKey(key);
+
+// send the mail by sendgrid library
 send1.send({
     to: 'ashoojain6@gmail.com',
     from: 'ashoojain6@gmail.com',
     subject: 'Sending Email',
     text: 'Hi Ashoo Hope you are doing well enjoy',
-}).then((tt)=>{
+}).then((tt)=>{      // if things goes well then we got sent
     console.log('sent');
-}).catch(e=>onsole.log(e));
+})
+     //otherwise got error
+    .catch(e=>onsole.log(e));
